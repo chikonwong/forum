c5aa2bb1843cde5ba7b4f67538462b0e604cf20d
Jacky Wong <chikonwong@gmail.com>
2021-05-01 22:59:55 +0800

    no message

---
+++ a_user/register.py
@@ -0,0 +1,17 @@
+from django.contrib.auth.forms import UserCreationForm
+from django.shortcuts import render
+
+from register_forms import CustomUserCreationForm
+
+
+def signup(request):
+    if request.method == 'POST':
+        form = CustomUserCreationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return request('index')
+
+    else:
+        form = CustomUserCreationForm()
+
+    return render(request, 'singup.html', {'form': form})
