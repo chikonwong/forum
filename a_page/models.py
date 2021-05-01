ef2c3e1e908776482978ada9a8e7e907dec34196
Jacky Wong <chikonwong@gmail.com>
2021-05-01 15:24:06 +0800

    no message

--- a_page/models.py
+++ a_page/models.py
@@ -1,7 +1,18 @@
+import datetime
+
 from django.db import models
+from django.utils import timezone
 
 
 # Create your models here.
 
 class Page(models.Model):
     page_name = models.CharField(max_length=30)
+
+    # pub_date = models.DateTimeField('date publish')
+
+    def __str__(self):
+        return self.page_name
+
+    # def was_published_recently(self):
+    #     return self.pub_date >= timezone.now() - datetime.timedelta(days=1)
--- a_page/views.py
+++ a_page/views.py
@@ -1,8 +1,15 @@
 from django.http import HttpResponse
 from django.shortcuts import render
+from a_page.models import Page
 
 
 # Create your views here.
 
+# def index(request):
+#     latest_page_list = Page.objects.order_by('pub_date')
+#     context = {'latest_page_list': latest_page_list}
+#     return render(request, 'index.html', context)
 def index(request):
-    return HttpResponse("start")
+    latest_page_list = Page.objects.order_by('page_name')
+    context = {'latest_page_list': latest_page_list}
+    return render(request, 'index.html', context)
