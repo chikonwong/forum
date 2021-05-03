574937b2ab872617a2ce3ed35b37277ed30f61f4
Jacky Wong <chikonwong@gmail.com>
2021-05-03 16:05:00 +0800

    no message

--- a_page/models.py
+++ a_page/models.py
@@ -1,7 +1,7 @@
 import datetime
 import uuid
 
-from django.contrib.auth.models import Group
+from django.contrib.auth.models import Group, User
 from django.db import models
 from django.utils import timezone
 
@@ -30,3 +30,26 @@ class PageAdminGroup(models.Model):
 
     def __str__(self):
         return self.group
+
+
+class UserPost(models.Model):
+    user_id = models.ForeignKey(User, on_delete=models.CASCADE)
+    post_title = models.ForeignKey(Post, on_delete=models.CASCADE)
+
+
+class Post(models.Model):
+    status = {
+        (0, 'Inactive'),
+        (1, 'Active')
+    }
+    post_id = models.UUIDField(primary_key=True, default=uuid.uuid4(), editable=False)
+    post_title = models.CharField(max_length=30)
+    page_priority = models.IntegerField(default=50)
+    page_status = models.SmallIntegerField(choices=status, default=0)
+    created_at = models.DateTimeField(auto_now_add=True)
+    modified_at = models.DateTimeField(auto_now=True)
+    post_author_id = models.ForeignKey(UserPost, on_delete=models.CASCADE)
+    post_content = models.TextField()
+
+    def __str__(self):
+        return self.post_title
