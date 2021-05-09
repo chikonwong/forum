79245d626ff8b17bd7ba626644d86dec444b73dc
Jacky Wong <chikonwong@gmail.com>
2021-05-09 12:05:10 +0800

    no message

--- forum/urls.py
+++ forum/urls.py
@@ -37,5 +37,4 @@ urlpatterns = [
     path('home/others/', auth_views.LoginView.as_view(template_name='others.html'), name='others'),
     path('signup/', views.signup, name='signup'),
     path('logout/', auth_views.LogoutView.as_view(template_name='logout.html'), name='logout')
-
 ]
